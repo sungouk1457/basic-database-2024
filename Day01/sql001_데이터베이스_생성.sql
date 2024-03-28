f343a8488c77a43e0a23d57dd6ddcef780f9bde7
sungouk1457 <sungouk1457@naver.com>
2024-03-28 15:56:32 +0900

    sql1일차

---
+++ Day01/sql001_데이터베이스_생성.sql
@@ -0,0 +1,4 @@
+--이게 주석입니다.
+/*C/C++ 주석도 가능*/
+-- 실행은 F5
+CREATE DATABASE basic2024;
---
+++ Day01/sql003_예제 실행.sql
@@ -0,0 +1,12 @@
+-- 책중에서 '축구의 역사'라는 도서의 출판사와  가격을 알고 싶어
+-- dbo는 DataBase Owner(지금은 패스)
+-- DML(4가지)중 SELECT (조회)
+SELECT publisher, price
+  FROM Book
+ WHERE bookname = '축구의 역사';
+/*
+- SQL에서는 equal연산자에 == 사용안함,무조건 =
+- SQL에서는 문자열에 "사용안함,무조건 '
+- SQL에서는 대소문자 구분없음, 하지만 키워드는 대문자로 사용할것
+- SQL에서는 ;이 필수가 아님, 하지만 중요한 사항에서는 무조건 사용할 것
+*/
